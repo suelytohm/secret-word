5f92a3aa8e8974d5bb20223218eedd19c8ba96ed
suelytohm <suelytohm.02@gmail.com>
2022-08-25 12:37:44 -0300

    alteração de dados das palavras

--- src/data/words.js
+++ src/data/words.js
@@ -2,5 +2,5 @@ export const wordsList = {
     carro: ["Motor", "Porta", "Capô", "Pneu", "Freio", "Acelerador"],
     fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Melancia"],
     corpo: ["Braço", "Perna", "Cabeça", "Pescoço", "Olhos"],
-    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Queijo", "Bolo", "Torta", "Macarrão", "Bolacha", "Pão", "Café", "Farofa", "Feijoada"]
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Queijo", "Bolo", "Torta", "Macarrão", "Bolacha", "Café", "Farofa", "Feijoada"]
 };
